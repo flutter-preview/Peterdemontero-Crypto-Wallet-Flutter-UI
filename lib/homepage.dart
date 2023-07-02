9d6a962e020d2bdcc31c16dc161cca780123e1fc
PeterDemonteroe <peterdemontero@gmail.com>
2023-07-02 01:54:14 +0000

    first commit on branch

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,12 +1,40 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Color(0xfff221f3a),
+      bottomNavigationBar: CurvedNavigationBar(
+          backgroundColor: Color(0xfff221f3a),
+          color: Color(0xff301B57),
+          animationDuration: const Duration(milliseconds: 400),
+          items: [
+            Image.asset(
+              'lib/icons/history.png',
+              height: 40.0,
+            ),
+            Image.asset(
+              'lib/icons/currency-exchange.png',
+              height: 40.0,
+            ),
+            Image.asset(
+              'lib/icons/payment.png',
+              height: 40.0,
+            ),
+            Image.asset(
+              'lib/icons/person.png',
+              height: 40.0,
+            ),
+          ]),
       body: Center(
         child: Text(
           'Welcome To HomePage',
